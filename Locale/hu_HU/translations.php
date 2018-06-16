1924c1c9180169bb11329e6ab40bae1f448ffeb4
Balázs Úr <urbalazs@gmail.com>
2018-06-16 17:29:53 +0200

    Updated Hungarian translation

---
+++ Locale/hu_HU/translations.php
@@ -0,0 +1,10 @@
+<?php
+
+return [
+    'Broadcast Message' => 'Üzenet mindenkinek',
+    'Announcement' => 'Közlemény',
+    'Clear message' => 'Üzenet törlése',
+    'Publish' => 'Közzététel',
+    'Broadcast Message by Email' => 'Üzenet mindenkinek e-mailben',
+    'Send to everyone' => 'Küldés mindenkinek',
+];
